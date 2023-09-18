fc418351955b7f94c867503d4a340bc43e92170c
Kamal <kamalzulyansyah@gmail.com>
2023-09-18 19:31:36 +0700

    Update Tugas Testing Mocha

--- Penilaian.js
+++ Penilaian.js
@@ -10,37 +10,37 @@ let input = prompt("Masukkan nilai angka anda : ");
 angka = parseInt(input);
 
 
-if (input === "" ){
-    console.log("Angka tidak valid, silahkan coba lagi");
-}
-else {
-    angka = input;
-    if (angka < 0 || angka > 100){
-        console.log("Angka tidak valid, silahkan coba lagi");
-        grading(angka);
-    }
-    else{
-        console.log(typeof(angka))
-        grading(angka);
-    }
-        }
+// if (input === "" ){
+//     console.log("Angka tidak valid, silahkan coba lagi");
+//     print();
+// }
+// else {
+//     angka = input;
+//     if (angka < 0 || angka > 100){
+//         console.log("Angka tidak valid, silahkan coba lagi");
+//         grading(angka);
+//     }
+//     else{
+//         grading(angka);
+//     }
+//         }
 
 
-// if(Number.isInteger(angka)){
-//     grading(angka)
-//         // if (angka < 0 || angka > 100){
-//         //     console.log("Angka tidak valid, silahkan coba lagi");
-//         //     print();
-//         // }
-//         // else{
-//         //     grading(angka);
-//         // }
-//     }
-//         else{
-//         console.log("Ini bukan angka")
-//         console.log(typeof angka)
-//     print();   
-//     }
+if(Number.isInteger(angka)){
+    grading(angka)
+        // if (angka < 0 || angka > 100){
+        //     console.log("Angka tidak valid, silahkan coba lagi");
+        //     print();
+        // }
+        // else{
+        //     grading(angka);
+        // }
+    }
+        else{
+        console.log("Ini bukan angka")
+        console.log(typeof angka)
+    print();   
+    }
 
      }
 }
