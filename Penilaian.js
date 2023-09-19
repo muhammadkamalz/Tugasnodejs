30261a4f72bc6eee3dcb3f427bfbd38dad9360fe
Kamal <kamalzulyansyah@gmail.com>
2023-09-19 10:43:17 +0700

    Update Tugas

--- Penilaian.js
+++ Penilaian.js
@@ -3,45 +3,20 @@ const prompt = require('prompt-sync')()
 
 const print = () => {
 
-let angka = null;
-while (angka === null){
-
-let input = prompt("Masukkan nilai angka anda : ");
-angka = parseInt(input);
-
-
-// if (input === "" ){
-//     console.log("Angka tidak valid, silahkan coba lagi");
-//     print();
-// }
-// else {
-//     angka = input;
-//     if (angka < 0 || angka > 100){
-//         console.log("Angka tidak valid, silahkan coba lagi");
-//         grading(angka);
-//     }
-//     else{
-//         grading(angka);
-//     }
-//         }
-
-
-if(Number.isInteger(angka)){
-    grading(angka)
-        // if (angka < 0 || angka > 100){
-        //     console.log("Angka tidak valid, silahkan coba lagi");
-        //     print();
-        // }
-        // else{
-        //     grading(angka);
-        // }
+    let angka = parseFloat(prompt("masukkan nilai anda : "));
+    if(isNaN(angka)){
+        console.log("Gagal");
+        print();
     }
-        else{
-        console.log("Ini bukan angka")
-    print();   
+    // else if (angka < 0 || angka > 100){
+    //     grading(angka);
+    //     print();
+    // }
+    else {
+        grading(angka);
     }
 
-     }
+    
 }
 print();
 
